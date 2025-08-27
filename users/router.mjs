d039ba28d2d8ef92ef41964a6b2338e96151bde1
Excel <000.excel@gmail.com>
2025-08-27 07:38:32 +0530

    router setup

---
+++ users/router.mjs
@@ -0,0 +1,9 @@
+import express from 'express';
+const userRouter = express.Router();
+import { login, signup } from './controller.mjs'
+
+userRouter
+  .post('/signup', signup)
+  .post('/login', login)
+
+export default userRouter
