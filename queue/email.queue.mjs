2fbaa3afa18ecd2027b7c736559fe174243ee4da
Excel <000.excel@gmail.com>
2025-09-08 09:20:05 +0530

    queue email sender added

---
+++ queue/email.queue.mjs
@@ -0,0 +1,14 @@
+import { Queue } from 'bullmq'
+import redis from '../redis.mjs'
+
+const emailQueue = new Queue("email_queue", {
+  connection: redis
+  // defaultJobOptions: {
+  //   attempts: 3,
+  //   backoff: {
+  //     type: 'exponential', delay: 5000
+  //   },
+  // }
+})
+
+export default emailQueue
