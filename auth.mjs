66810779b95092b5ae960514ff78ceddced0bcb6
Excel <000.excel@gmail.com>
2025-09-12 07:49:42 +0530

    game routes added

--- auth.mjs
+++ auth.mjs
@@ -1,8 +1,9 @@
 const authentication = async (req, res, next) => {
+  console.log("auth")
   // 1. check for token is available
   // 2. validate token
   // 3. extract playload of token
-  // 4. attach to request for further use
+  // 4. attach user details to request for further use
   next()
 }
 
