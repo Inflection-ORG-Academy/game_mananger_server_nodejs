c03d7d61e8179756213b4e796b2fa03a3ddf4a1f
Excel <000.excel@gmail.com>
2025-09-16 09:14:29 +0530

    multer config added

--- users/router.mjs
+++ users/router.mjs
@@ -1,7 +1,8 @@
 import express from 'express';
 const userRouter = express.Router();
-import { forgotPassword, getMe, login, resetPassword, signup } from './controller.mjs'
+import { forgotPassword, getMe, login, resetPassword, signup, updateProfileImage } from './controller.mjs'
 import { authentication } from '../auth.mjs';
+import { singleImageUploadMiddleware } from '../storage/config.mjs';
 
 userRouter
   .post('/signup', signup)
@@ -9,5 +10,6 @@ userRouter
   .patch('/forgotPassword', forgotPassword)
   .patch('/resetPassword', resetPassword)
   .get('/profile', authentication, getMe)
+  .patch('/profile/image', authentication, singleImageUploadMiddleware("image"), updateProfileImage)
 
 export default userRouter
