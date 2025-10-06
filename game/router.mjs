657248ec49eb0531a68e09d759e4baab65735916
Excel <000.excel@gmail.com>
2025-10-06 07:55:12 +0530

    added authorization

--- game/router.mjs
+++ game/router.mjs
@@ -1,12 +1,12 @@
 import express from 'express';
 const gameRouter = express.Router();
 import { addGame, listGame, requestGame, getMyGameSession } from "./controller.mjs"
-import { authentication } from '../auth.mjs';
+import { authentication, authorization } from '../auth.mjs';
 
 gameRouter.use(authentication)
 
 gameRouter
-  .post('/', addGame)
+  .post('/', authorization('ADMIN'), addGame)
   .get('/', listGame)
   .post('/request', requestGame)
   .get('/session/:sessionID', getMyGameSession)
